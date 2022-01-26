34f19b107a8e5d5dae44372d7bbdc3b09be70904
Ryan Murry <ryanjmurry@gmail.com>
2022-01-25 18:22:49 -0800

    chore: car shop and car movement

---
+++ car_shop.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+from constants import SCREEN_H, SCREEN_W
+from random import randint, choice
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+MAX_Y = SCREEN_H // 2 - 50
+MIN_Y = -MAX_Y + 50
+STARTING_X = SCREEN_W // 2
+
+
+class CarShop:
+    def __init__(self, level):
+        self.cars = []
+        self.level = level
+        self.move_distance = STARTING_MOVE_DISTANCE + (MOVE_INCREMENT * self.level)
+
+    def new_car(self):
+        new_car = Turtle(shape="square")
+        new_car.right(180)
+        new_car.color(choice(COLORS))
+        new_car.pu()
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        rand_y = randint(MIN_Y, MAX_Y)
+        new_car.goto(STARTING_X, rand_y)
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(self.move_distance)
--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import time
 from turtle import Screen
 from constants import SCREEN_W, SCREEN_H
 from frog import Frog
+from car_shop import CarShop
 
 screen = Screen()
 screen.tracer(0)
 screen.setup(width=SCREEN_W, height=SCREEN_H)
 
 froggy = Frog()
+car_shop = CarShop(level=0)
 
 screen.listen()
 screen.onkey(froggy.move, "Up")
 
 game_in_progress = True
+game_loops = 0
 
 while game_in_progress:
+    if game_loops % 6 == 0:
+        car_shop.new_car()
+
+    car_shop.move_cars()
+
     time.sleep(0.1)
     screen.update()
+    game_loops += 1
 
 screen.exitonclick()
