15ef847157800793fd93a769a9fa20a39de2dd68
Ryan Murry <ryanjmurry@gmail.com>
2022-01-25 17:45:51 -0800

    chore: initial frog class + move behavior

---
+++ frog.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Frog(Turtle):
+    def __init__(self):
+        super().__init__(shape="turtle")
+        self.left(90)
+        self.pu()
+        self.goto(STARTING_POSITION)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import time
 from turtle import Screen
 from constants import SCREEN_W, SCREEN_H
+from frog import Frog
 
 screen = Screen()
 screen.tracer(0)
 screen.setup(width=SCREEN_W, height=SCREEN_H)
+
+froggy = Frog()
+
+screen.listen()
+screen.onkey(froggy.move, "Up")
 
 game_in_progress = True
 
